afcfedbbdd53e88b8a3419d2c608452ea6bb946f
Vitaly <sushiov.vit@gmail.com>
2021-12-14 08:50:54 +0200

    Add smart enum prototype

--- PrototypeUI/Program.cs
+++ PrototypeUI/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,10 +16,61 @@ namespace PrototypeUI
         [STAThread]
         static void Main()
         {
+
+            Debug.WriteLine("Roles:");
+            foreach (var role in Role.List())
+            {
+                Debug.WriteLine($"Role: {role.Name} ({role.Value})");
+            }
+
+
+            Debug.WriteLine("Roles 2:");
+
+            foreach (var role in Role.AllRoles)
+            {
+                Debug.WriteLine($"Role: {role.Name} ({role.Value})");
+            }
+
+            Debug.WriteLine("Job Titles:");
+
+            foreach (var title in JobTitle.AllTitles)
+            {
+                Debug.WriteLine($"Title: {title.Name} ({title.Value})");
+            }
+
+            Debug.WriteLine("--Smart Enums--");
+            Debug.WriteLine("Smart Foo:");
+            foreach (var smartFoo in SmartFoo.List)
+            {
+                Debug.WriteLine($"Foo: {smartFoo}");
+            }
+            Debug.WriteLine("Smart Bar:");
+            foreach (var smartBar in SmartBar.List)
+            {
+                Debug.WriteLine($"Bar: {smartBar}");
+            }
+            Debug.WriteLine("Smart Baz:");
+            foreach (var smartBaz in SmartBaz.List)
+            {
+                Debug.WriteLine($"Baz: {smartBaz}");
+            }
+
+            var module = SpaceShipModules.Shields;
+
+            var xxx = SpaceShipModules.Weapon.Value;
+
+            var xx = WeaponModules.Missiles;
+
+            var battleship = SpaceShips.Battleship;
+
+            var x = battleship.Value;
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
+
+    
 }
---
+++ PrototypeUI/SpaceShips.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using Ardalis.SmartEnum;
+
+namespace PrototypeUI
+{
+    class Tests
+    {
+        private static void V()
+        {
+            //SpaceShips.Battleship.Value.
+            //
+        }
+
+    }
+
+    public struct A
+    {
+        public StarShipsTypes Types;
+    }
+
+    public enum StarShipsTypes
+    {
+        Battleship,
+        Frigate
+    }
+
+    public enum StarShipsModules
+    {
+        Weapon,
+        Shields
+    }
+
+    public sealed class SpaceShips: SmartEnum<SpaceShips>
+    {
+        public static readonly SpaceShips Battleship = new SpaceShips("Battleship", 1, SpaceShipModule.AllModules);
+        public static readonly SpaceShips Frigate = new SpaceShips("Frigate", 1, SpaceShipModule.AllModules);
+
+        public List<SpaceShipModule> Modules { get; set; }
+
+        public SpaceShips(string name, int cannons, List<SpaceShipModule> modules) : base(name, cannons)
+        {
+            Modules = modules;
+        }
+
+    }
+
+    public sealed class SpaceShipModule : SmartEnum<SpaceShipModule>
+    {
+        public static List<SpaceShipModule> AllModules { get; } = new List<SpaceShipModule>();
+
+        public static readonly SpaceShipModule Shields = new SpaceShipModule("Shields", 100);
+        public static readonly SpaceShipModule Weapon = new SpaceShipModule("Weapon", 20);
+
+        private SpaceShipModule(string name, int cannons) : base(name, cannons)
+        {
+            AllModules.Add(this);
+        }
+    }
+}
