f74aed29470c1928a014bee4e0ddbd5b030bf0e8
SuhilovVitaly <98092169+SuhilovVitaly@users.noreply.github.com>
2022-04-14 12:28:35 +0300

    [OSP-8] Add NUnit test generation system

--- OutlandSpaceUniverseTests/Objects/Asteroids/AsteroidTests.cs
+++ OutlandSpaceUniverseTests/Objects/Asteroids/AsteroidTests.cs
@@ -1,8 +1,6 @@
 ﻿using NUnit.Framework;
+using Universe.Objects;
 using Universe.Objects.Asteroids;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Tests.Universe.Objects.Asteroids
 {
@@ -12,7 +10,17 @@ namespace Tests.Universe.Objects.Asteroids
         [Test()]
         public void AsteroidTest()
         {
-            throw new NotImplementedException();
+            // Arrange
+
+            var expectedAsteroidType = CelestialObjectTypes.Asteroid;
+
+            // Act
+
+            var asteroid = new Asteroid();
+
+            // Assert
+
+            Assert.AreEqual(expectedAsteroidType, asteroid.Type);
         }
     }
 }
